d6854f982bde950259449e02169d98cc82a31e68
Aras Bazyan <aras.bazyan@gmail.com>
2016-11-23 10:55:38 +0100

    custom adapter

--- app/src/main/java/com/gu/example/axel/baldr/CustomAdapter.java
+++ app/src/main/java/com/gu/example/axel/baldr/CustomAdapter.java
@@ -3,6 +3,7 @@ package com.gu.example.axel.baldr;
 import android.app.Activity;
 import android.content.Context;
 import android.provider.Settings;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -13,6 +14,8 @@ import android.widget.TextView;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.MqttTopic;
 import org.w3c.dom.Text;
+
+import java.util.List;
 
 /**
  * Created by Axel on 06-Oct-16.
@@ -25,10 +28,13 @@ public class CustomAdapter extends BaseAdapter {
     LightObject[] data;
     MqttConnection sender;
 
+    public List<Json> lightItems;
+    public LayoutInflater inflater;
 
-    public CustomAdapter(Context context, LightObject[] data){
+
+    public CustomAdapter(Context context, List<Json> lightItems){
         this.context = context;
-        this.data = data;
+        this.lightItems = lightItems;
         sender = new MqttConnection(context);
         sender.connect();
 
@@ -36,23 +42,35 @@ public class CustomAdapter extends BaseAdapter {
 
     @Override
     public int getCount() {
-        return data.length;
+
+        return lightItems.size();
     }
 
     @Override
-    public Object getItem(int position) {
-        return data[position];
+    public Object getItem(int position)
+    {
+        return lightItems.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return position;
+
+        return 0;
     }
 
 
     @Override
-    public View getView(final int position, View convertView, ViewGroup parent) {
-        View vi = View.inflate(context, R.layout.light_row, null);
+    public View getView( final int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+
+        View rowView = inflater.inflate(R.layout.light_row, parent,false);
+        Json items = (Json) getItem(position);
+        TextView lName = (TextView) rowView.findViewById(R.id.lightName);
+        //TextView lRoom = (TextView) rowView.findViewById(R.id.lightRoom);
+        Switch lSwitch = (Switch) rowView.findViewById(R.id.lightSwitch);
+
+/*        View vi = View.inflate(context, R.layout.light_row, null);
         TextView lName = (TextView) vi.findViewById(R.id.lightName);
         TextView lRoom = (TextView) vi.findViewById(R.id.lightRoom);
         Switch lSwitch = (Switch) vi.findViewById(R.id.lightSwitch);
@@ -74,7 +92,9 @@ public class CustomAdapter extends BaseAdapter {
 
 
             }
-        });
+        });*/
+
+        lName.setText(items.getLightInfo().getRoom());
 
         lSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -99,7 +119,7 @@ public class CustomAdapter extends BaseAdapter {
             }
         });
 
-        return vi;
+        return rowView;
     }
 
 
